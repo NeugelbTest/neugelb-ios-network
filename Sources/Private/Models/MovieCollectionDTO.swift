e371fa129b1d3d12abb80ed788a0480f168857b7
Matt Moayeri <notron021@gmail.com>
2025-03-14 20:41:39 +0100

    Added MovieCompleteDTO

---
+++ Sources/Private/Models/MovieCollectionDTO.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+struct MovieCollectionDTO: Decodable {
+    let id: Int
+    let name: String
+    let posterPath: String?
+    let backdropPath: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case posterPath = "poster_path"
+        case backdropPath = "backdrop_path"
+    }
+}
---
+++ Sources/Private/Models/MovieCompleteDTO.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+struct MovieCompleteDTO: Decodable {
+    let id: Int
+    let title: String
+    let overview: String
+    let releaseDate: String?
+    let rating: Double?
+    let voteCount: Int?
+    let runtime: Int?
+    let budget: Int?
+    let revenue: Int?
+    let status: String?
+    let adult: Bool?
+    let originalLanguage: String?
+    let genres: [GenreDTO]
+    let belongsToCollection: MovieCollectionDTO?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title, overview, runtime, budget, revenue, status, adult, genres
+        case releaseDate = "release_date"
+        case rating = "vote_average"
+        case voteCount = "vote_count"
+        case originalLanguage = "original_language"
+        case belongsToCollection = "belongs_to_collection"
+    }
+}
+
+extension MovieCompleteDTO {
+
+    func toMovie() -> Movie {
+        return Movie.Builder()
+            .with(movieCompleteDTO: self)
+            .build()
+    }
+}
