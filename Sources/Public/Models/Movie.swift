d6a111493865c64c7871ba1b8f79ca4982bc5ead
Matt Moayeri <notron021@gmail.com>
2025-03-14 20:42:06 +0100

    Add more entities to Movie

--- Sources/Public/Models/Movie.swift
+++ Sources/Public/Models/Movie.swift
@@ -6,21 +6,14 @@ public struct Movie: Hashable, Identifiable {
     public let title: String
     public let overview: String
     public let poster: String?
-    public let rating: Double
+    public let cover: String?
+    public let rating: Double?
     public let releaseDate: Date?
     public let language: String?
-}
-
-extension Movie {
-
-    init(_ movieDTO: MovieDTO) {
-
-        id = movieDTO.id
-        title = movieDTO.title
-        overview = movieDTO.overview
-        poster = movieDTO.posterPath
-        rating = movieDTO.voteAverage
-        releaseDate = Date(serverTime: movieDTO.releaseDate)
-        language = movieDTO.language
-    }
+    public var runtime: Int?
+    public var budget: Int?
+    public var revenue: Int?
+    public var status: String?
+    public var adult: Bool?
+    public var genres: [Genre] = []
 }
